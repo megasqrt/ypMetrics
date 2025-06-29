213a5acdc02dec3aacda5069075486a7e89a71ca
megasqrt <my@mail.com>
2025-06-29 08:16:28 +0300

    fix npe

--- internal/services/handler_test.go
+++ internal/services/handler_test.go
@@ -12,26 +12,76 @@ import (
 )
 
 
-func TestMetricServer_updateHandler2(t *testing.T) {
+func TestUpdateHandler(t *testing.T) {
 	type want struct {
-        statusCode  int
-		mType string
-		mName string
-		mValue string
-		body string
-    }
+		statusCode int
+		body       string
+	}
 	tests := []struct {
 		name   string
+		mType  string
+		mName  string
+		mValue string
 		want   want
 	}{
 		{
-			name: "Valid Gauge",
+			name:   "Valid Gauge",
+			mType:  "gauge",
+			mName:  "Latency",
+			mValue: "10.5",
 			want: want{
-				statusCode:        http.StatusOK,
-				mType: 	"gauge",
-				mName: 	"Latency",
-				mValue: "10",
-				body: `Gauge Latency updated to`,
+				statusCode: http.StatusOK,
+				body:       "Gauge Latency updated to",
+			},
+		},
+		{
+			name:   "Valid Counter",
+			mType:  "counter",
+			mName:  "Requests",
+			mValue: "5",
+			want: want{
+				statusCode: http.StatusOK,
+				body:       "Counter Requests incremented by 5",
+			},
+		},
+		{
+			name:   "Invalid Gauge Value",
+			mType:  "gauge",
+			mName:  "Latency",
+			mValue: "abc",
+			want: want{
+				statusCode: http.StatusBadRequest,
+				body:       "Invalid gauge value\n",
+			},
+		},
+		{
+			name:   "Invalid Counter Value",
+			mType:  "counter",
+			mName:  "Requests",
+			mValue: "abc",
+			want: want{
+				statusCode: http.StatusBadRequest,
+				body:       "Invalid counter value\n",
+			},
+		},
+		{
+			name:   "Invalid Metric Type",
+			mType:  "unknown",
+			mName:  "SomeMetric",
+			mValue: "123",
+			want: want{
+				statusCode: http.StatusBadRequest,
+				body:       "Invalid metric type unknown\n",
+			},
+		},
+		{
+			name:   "Missing Metric Name",
+			mType:  "gauge",
+			mName:  "",
+			mValue: "10",
+			want: want{
+				statusCode: http.StatusNotFound,
+				body:       "Invalid URL format\n",
 			},
 		},
 	}
@@ -39,18 +89,17 @@ func TestMetricServer_updateHandler2(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			mockStorage := &mocks.MockStorage{}
 			handler := NewHandler(mockStorage)
-			request,_ := http.NewRequest(http.MethodPost, "/update", nil)
+			request, _ := http.NewRequest(http.MethodPost, "/update", nil)
 			record := httptest.NewRecorder()
 			vars := map[string]string{
-				"type":  tt.want.mType,
-				"name":  tt.want.mName,
-				"value": tt.want.mValue,
+				"type":  tt.mType,
+				"name":  tt.mName,
+				"value": tt.mValue,
 			}
 			request = mux.SetURLVars(request, vars)
-			t.Log(request)
-            handler.updateHandler(record, request)
+			handler.updateHandler(record, request)
 
-			assert.Equal(t, http.StatusOK, record.Code)
+			assert.Equal(t, tt.want.statusCode, record.Code)
 			assert.Contains(t, record.Body.String(), tt.want.body)
 		})
 	}
@@ -58,7 +107,10 @@ func TestMetricServer_updateHandler2(t *testing.T) {
 
 func TestGetMetricHandler(t *testing.T) {
 	
-	mockStorage := &mocks.MockStorage{}
+	mockStorage := &mocks.MockStorage{
+		Gauges:   make(map[string]float64),
+		Counters: make(map[string]int64),
+	}
 	mockStorage.WithGauge( "temperature", 36.6).
 				WithCounter("requests", 42)
     
@@ -140,4 +192,3 @@ func TestGetMetricHandler(t *testing.T) {
 		})
 	}
 }
-
